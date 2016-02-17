5d16493282d4f02268384adf44f36069e982b55e
Tan Choi <hoantas@gmail.com>
2016-02-18 04:15:34 +0900

    now ajax/article is available all or some category response

--- src/main/java/com/devTeam/versiona/AriticleController.java
+++ src/main/java/com/devTeam/versiona/AriticleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.devTeam.versiona.service.ArticleService;
+import com.devTeam.versiona.utils.ArticleCategory;
 import com.devTeam.versiona.utils.Constant;
 
 /**
@@ -32,9 +33,27 @@ public class AriticleController {
     @RequestMapping(value = "/ajax/article", method = RequestMethod.GET)
     public @ResponseBody Map<?,?> articleAjax(@RequestParam Map<String, String> paramMap, ModelMap model) throws Throwable {
 
+        if(isAllCategory(paramMap)) {
+            
+            model.put("articles", articleService.getAllCategoryArticle(Integer.parseInt(paramMap.get("PAGE_ID"))));
+            
+        } else {
+            // Not all category
+            model.put("articles", articleService.getArticleByCategoryAndPage(paramMap.get("CATEGORY_ID").toString(), Integer.parseInt(paramMap.get("PAGE_ID"))));
+            
+        }
+        
         model.put("result", Constant.SUCCESS);
-        model.put("articles", articleService.getArticleByCategoryAndPage(paramMap.get("CATEGORY_ID").toString(), Integer.parseInt(paramMap.get("PAGE_ID"))));
         return model;
-
+        
+    }
+    
+    private boolean isAllCategory(Map<String, String> paramMap) {
+        
+        if(paramMap.get("CATEGORY_ID").toString().equals(ArticleCategory.ALL.getName()))
+            return true;
+        
+        return false;
+        
     }
 }
--- src/main/java/com/devTeam/versiona/utils/ArticleCategory.java
+++ src/main/java/com/devTeam/versiona/utils/ArticleCategory.java
@@ -3,7 +3,7 @@ package com.devTeam.versiona.utils;
 public enum ArticleCategory {
 
     // need to sync database ENUM category.
-    GETSTARTED("Get Started"), DEVSTORY("개발자 이야기"), TIPNTECH("팁&테크"), ETC("기타");
+    ALL("모든 아티클"), GETSTARTED("Get Started"), DEVSTORY("개발자 이야기"), TIPNTECH("팁&테크"), ETC("기타");
 
     private String name;
 
